92036970a390f450ead645d35643626cc1d783d3
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-07-31 13:20:39 +0500

    finalized

---
+++ app.js
@@ -0,0 +1,21 @@
+const displayBlock = document.getElementById('display-block')
+
+function getNum(num){
+    displayBlock.value += num
+}
+
+function clearAll(){
+    displayBlock.value = " "
+}
+
+function clearValue(){
+    const tempValue = displayBlock.value
+    displayBlock.value = displayBlock.value.slice(0,(tempValue.length-1))
+    
+}
+
+function calculate(){
+   displayBlock.value = eval(displayBlock.value);
+
+}
+
